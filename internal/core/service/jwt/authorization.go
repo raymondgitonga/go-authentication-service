eb8778d63cdc19b5247da1c2051a27380e5c78dd
raymondgitonga <raytosh95@gmail.com>
2022-12-23 16:54:36 +0000

    parse jwt

--- internal/core/service/jwt/authorization.go
+++ internal/core/service/jwt/authorization.go
@@ -7,6 +7,8 @@ import (
 	"time"
 )
 
+var secret = []byte("")
+
 type Authorization struct {
 	authRequest dormain.AuthRequest
 }
@@ -22,11 +24,30 @@ func (a *Authorization) Authorize() (string, error) {
 		Subject:   a.authRequest.Email,
 	}
 
-	tokenString, err := generateToken(claims, []byte(""))
+	tokenString, err := generateToken(claims, secret)
 	if err != nil {
 		return "", nil
 	}
 	return tokenString, nil
+}
+
+func parseToken(signedToken string) (*jwt.StandardClaims, error) {
+	token, err := jwt.ParseWithClaims(signedToken, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		if t.Method.Alg() != jwt.SigningMethodHS256.Alg() {
+			return nil, fmt.Errorf("error in Validate wrong signing algo used")
+		}
+		return secret, nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error in Validate, error parsing token: %w", err)
+	}
+
+	if !token.Valid {
+		return nil, fmt.Errorf("error in Validate: token not valid")
+	}
+
+	return token.Claims.(*jwt.StandardClaims), nil
 }
 
 func generateToken(claims jwt.StandardClaims, secret []byte) (string, error) {
--- internal/core/service/jwt/authorization_test.go
+++ internal/core/service/jwt/authorization_test.go
@@ -18,3 +18,18 @@ func TestGenerateToken(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzE4OTQxMzEsImlhdCI6MTY3MTgwNzczMSwic3ViIjoiZW1haWxAZ21haWwuY29tIn0.pPGxkoXX8AE861CVvjTvf5lhLGJwRCRyhWZLDeQauGY", token)
 }
+
+func TestParseToken(t *testing.T) {
+	claims := jwt.StandardClaims{
+		IssuedAt:  1671807731,
+		ExpiresAt: 1671894131,
+		Subject:   "email@gmail.com",
+	}
+
+	secret := []byte("")
+	token, err := generateToken(claims, secret)
+	claim, err := parseToken(token)
+
+	assert.Equal(t, "email@gmail.com", claim.Subject)
+	assert.NoError(t, err)
+}
