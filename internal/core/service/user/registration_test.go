ccdc2d0ce8461bd29d31461071cdf2218b30493a
raymondgitonga <raytosh95@gmail.com>
2022-12-25 13:17:48 +0000

    mock and test repository

---
+++ internal/core/service/user/registration_test.go
@@ -0,0 +1,44 @@
+package user_test
+
+import (
+	"fmt"
+	"github.com/raymondgitonga/go-authentication/internal/core/service/user"
+	"github.com/raymondgitonga/go-authentication/internal/core/service/user/mocks"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"testing"
+)
+
+func TestRegistrationService_RegisterUser(t *testing.T) {
+	logger, err := zap.NewProduction()
+	assert.NoError(t, err)
+	defer func() {
+		err = logger.Sync()
+	}()
+	assert.NoError(t, err)
+
+	t.Run("successfully add user", func(t *testing.T) {
+		repo := &mocks.RepositoryMock{AddUserFunc: func(name string, secret []byte) error {
+			return nil
+		}}
+
+		service := user.NewRegistrationService(repo, logger)
+
+		auth, err := service.RegisterUser("example-service")
+		assert.NoError(t, err)
+		assert.Equal(t, "example-service", auth.Name)
+		assert.Equal(t, 36, len(auth.Secret))
+	})
+
+	t.Run("failed to add user", func(t *testing.T) {
+		repo := &mocks.RepositoryMock{AddUserFunc: func(name string, secret []byte) error {
+			return fmt.Errorf("error adding user")
+		}}
+
+		service := user.NewRegistrationService(repo, logger)
+
+		auth, err := service.RegisterUser("example-service")
+		assert.Error(t, err)
+		assert.Nil(t, auth)
+	})
+}
